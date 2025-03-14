879e8ab032ef58e62d2324d41bbd6de0b34de463
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-03-14 13:40:47 +0330

    Add endpoint routes to main

--- services/Product/app/api/v1/routers.py
+++ services/Product/app/api/v1/routers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from app.api.v1.endpoint import company
 
-from api.v1.endpoints import product
+
 
 api_router = APIRouter()
 
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(user.router, prefix="/user", tags=["user"])
+
+api_router.include_router(company.router, prefix="/company", tags=["company"])
+
