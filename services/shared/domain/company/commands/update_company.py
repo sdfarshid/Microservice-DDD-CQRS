f14d93aa9cdb817bfcfe728cc84b7626fe3ecbae
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-05-08 11:57:20 +0330

    Create a shared folder so that we have a Monorepo - This step just add company service info (command - queries - mixin )

---
+++ services/shared/domain/company/commands/update_company.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
+
+class UpdateCompanyCommand(BaseModel):
+    company_id: UUID
+    name: Optional[str] = None
+    registration_number: Optional[str] = None
+    address: Optional[str] = None
+    website: Optional[str] = None
+    status: Optional[str] = None
+
+    def __str__(self):
+        return f"UpdateCompanyCommand(name={self.name}, id={self.company_id})"
