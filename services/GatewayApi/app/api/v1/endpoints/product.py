8e87e6435b6a39374d04474ab3f6f6f1a338e2a3
farshidmousavi <sdfarshidmousavi@gmail.com>
2025-07-03 19:36:37 +0330

    Feat (GateWay): using product endpoint from shared data

--- services/GatewayApi/app/api/v1/endpoints/product.py
+++ services/GatewayApi/app/api/v1/endpoints/product.py
@@ -6,6 +6,10 @@ from app.config.config import settings
 from app.utilities.log import DebugError, DebugWaring
 from app.utilities.helper import handle_exceptions, call_api
 
+from shared.domain.product.commands import CreateProductCommand
+from shared.domain.product.commands.update_product import UpdateProductRequest
+from shared.mixins.pagination import PaginationParams, get_pagination_params
+
 router = APIRouter(tags=["product"])
 
 PRODUCT_BASE_URL = settings.get_service_url("product")
@@ -17,10 +21,10 @@ async def get_product(product_id: UUID):
     return await call_api(method="GET", endpoint=f"{PRODUCT_BASE_URL}/{product_id}")
 
 
-@router.post("/")
+@router.post("")
 @handle_exceptions
-async def create_product(command: dict):
-    return await call_api(method="POST", endpoint=f"{PRODUCT_BASE_URL}/", json_data=command)
+async def create_product(command: CreateProductCommand):
+    return await call_api(method="POST", endpoint=f"{PRODUCT_BASE_URL}", json_data=command)
 
 
 @router.post("/reserve")
@@ -34,22 +38,22 @@ async def release_reserve_product(command: dict):
     return await call_api(method="PUT", endpoint=f"{PRODUCT_BASE_URL}/release", json_data=command)
 
 
-@router.get("/", response_model=List[dict])
+@router.get("", response_model=List[dict])
 @handle_exceptions
 async def list_products(
         company_id: Optional[UUID] = Query(None, description="ID of the company to filter products"),
-        page: int = Query(1, ge=1),
-        page_size: int = Query(10, ge=1, le=100)
+        pagination: PaginationParams = Depends(get_pagination_params)
 ):
-    params = {"page": page, "page_size": page_size}
+    params = pagination.model_dump()
     if company_id:
-        params["company_id"] = str(company_id)
+        params["company_id"] = company_id
+
     return await call_api(method="GET", endpoint=f"{PRODUCT_BASE_URL}", params=params)
 
 
 @router.put("/{product_id}")
 @handle_exceptions
-async def update_product(product_id: UUID, command: dict):
+async def update_product(product_id: UUID, command: UpdateProductRequest):
     return await call_api(method="PUT", endpoint=f"{PRODUCT_BASE_URL}/{product_id}", json_data=command)
 
 
